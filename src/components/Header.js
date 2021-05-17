608884003edfcb9b508db28840633738223c045c
Redouane Jnah <redouanejnah2000@gmail.com>
2021-05-17 22:02:42 +0100

    setup firebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components'
 import { links } from '../data'
+import { auth, provider } from '../firebase'
 
 const Header = () => {
+  const handleAuth = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  }
+
   return (
     <Nav>
       <Logo>
@@ -18,7 +30,7 @@ const Header = () => {
           )
         })}
       </NavMenu>
-      <Login>login</Login>
+      <Login onClick={handleAuth}>login</Login>
     </Nav>
   )
 }
