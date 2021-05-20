4363c4ea1c8afeebdb42dcb9b45909ab5ce6a93c
Redouane Jnah <redouanejnah2000@gmail.com>
2021-05-20 17:11:59 +0100

    add viewers component

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -78,20 +78,20 @@ const Wrapper = styled.div`
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
       rgb(0 0 0 / 73%) 0px 16px 10px -10px;
     cursor: pointer;
-    padding: 4px;
-    display: block;
     position: realitive;
-  }
+    display: block;
+    padding: 4px;
 
-  img {
-    width: 100%;
-    height: 100%;
-  }
+    img {
+      width: 100%;
+      height: 100%;
+    }
 
-  &:hover {
-    padding: 0;
-    border: 4px solid rgba(249, 249, 249, 0.8);
-    transition-duration: 300ms;
+    &:hover {
+      padding: 0;
+      border: 4px solid rgba(249, 249, 249, 0.8);
+      transition-duration: 300ms;
+    }
   }
 `
 
--- src/data.js
+++ src/data.js
@@ -50,3 +50,31 @@ export const sliders = [
     image: '/images/slider-scales.jpg',
   },
 ]
+
+export const viewers = [
+  {
+    id: 1,
+    image: '/images/viewers-disney.png',
+    video: '/videos/1564674844-disney.mp4',
+  },
+  {
+    id: 2,
+    image: '/images/viewers-marvel.png',
+    video: '/videos/1564676115-marvel.mp4',
+  },
+  {
+    id: 3,
+    image: '/images/viewers-national.png',
+    video: '/videos/1564676296-national-geographic.mp4',
+  },
+  {
+    id: 4,
+    image: '/images/viewers-pixar.png',
+    video: '/videos/1564676714-pixar.mp4',
+  },
+  {
+    id: 5,
+    image: '/images/viewers-starwars.png',
+    video: '/videos/1608229455-star-wars.mp4',
+  },
+]
